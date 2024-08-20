0423f802e6bae9bb81379f2fb943718e2f8285ad
NataleMarcos <marcosjaviernatale@gmail.com>
2024-08-20 03:07:49 -0300

    finished

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -25,7 +25,7 @@ return {
         config = function()
             vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
             vim.opt.undofile = true
-            vim.opt.undodir = "/home/angel/.config/nvim/undotree/"
+            vim.opt.undodir = os.getenv("HOME") .. "/.local/undodir"
         end
     },
     {
